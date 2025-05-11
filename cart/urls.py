a97a993eb5729c1c60afb587a5f16664b44a1f13
Reza Taheri <94985322+RezaTaheri01@users.noreply.github.com>
2025-05-12 00:35:36 +0330

    Update urls.py
    add graphql url

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+
+# GraphQL
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+from .schema import schema
+
+
 urlpatterns = [
     path('add-to-cart/', views.add_product_to_cart, name='add_to_cart'),
     path('request-payment/', views.request_payment, name='request_payment'),
@@ -11,4 +18,7 @@ urlpatterns = [
     path('api/cart/<int:pk>/', views.CartGenericApiView.as_view(), name='cart_api'),  # get user cart
     path('api/previous-purchase/<int:pk>/', views.PreviousCartGenericApiView.as_view(), name='previous-carts_api'),
     path('api/cart/product/<int:pk>/', views.CartProductGenericApiView.as_view(), name='product_in_cart_api'),
+
+    # GraphQL
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
